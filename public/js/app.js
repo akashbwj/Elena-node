6d24553e63580f2cc5ca277cbf874931363b710b
Akash Bhardwaj <akashbwj@gmail.com>
2021-06-26 02:01:45 +0530

    Add responsive home page

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,8 @@
-alert("hi")
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
