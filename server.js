dd78dbde8bd381d8ab49f3825e4579c985c938a8
Jeevan Kumar <rg1kumar1@gmail.com>
2021-07-12 21:40:50 +0530

    temporary removed DB

--- server.js
+++ server.js
@@ -3,35 +3,35 @@ require('dotenv').config()
 const express=require("express"),
     app=express(),
 	bodyParser=require("body-parser"),
-    mongoose=require("mongoose"),
-    mysql = require('mysql'),
-	passport=require('passport'),
-	LocalStrategy=require('passport-local'),
-    passportLocalMongoose=require('passport-local-mongoose'),
-	dbUrl=process.env.DB_URL||"mongodb://localhost:27017/gnss_india";
+    // mongoose=require("mongoose"),
+    // mysql = require('mysql'),
+	passport=require('passport');
+	// LocalStrategy=require('passport-local'),
+    // passportLocalMongoose=require('passport-local-mongoose'),
+	// dbUrl=process.env.DB_URL||"mongodb://localhost:27017/gnss_india";
 
     const flash=require("connect-flash");
 
 // Database Connection
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-});
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'password',
+// });
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log("DataBase Connected");
-    connection.query("CREATE DATABASE IF NOT EXISTS elena", function(err, result) {
-        if (err) throw err;
-        console.log("Database elena Created");
-    });
-    connection.query("USE elena", function(err, result) {
-        if (err) throw err;
-        console.log("Connected to DB elena!");
-    });
-})
+// connection.connect((err) => {
+//     if (err) throw err;
+//     console.log("DataBase Connected");
+//     connection.query("CREATE DATABASE IF NOT EXISTS elena", function(err, result) {
+//         if (err) throw err;
+//         console.log("Database elena Created");
+//     });
+//     connection.query("USE elena", function(err, result) {
+//         if (err) throw err;
+//         console.log("Connected to DB elena!");
+//     });
+// })
 
 app.use(bodyParser.urlencoded({extended:true}));
 
