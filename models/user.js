e3d5318be6d65fe81094251e16a9b78581ac8fe5
Jeevan Kumar <rg1kumar1@gmail.com>
2021-09-10 23:47:24 +0530

    Database Connected, Login/Register working!

--- models/user.js
+++ models/user.js
@@ -1,99 +1,70 @@
 
-// const mongoose = require('mongoose');
-// const joi = require('joi');
+const mongoose = require('mongoose');
+const joi = require('joi');
 
-// const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 
-// const dbUsername = process.env.dbUsername;
-// const dbPassword = process.env.dbPassword;
+const dbUsername = process.env.dbUsername;
+const dbPassword = process.env.dbPassword;
 
-// const sequelize = new Sequelize('elena-db', dbUsername, dbPassword, {
-//     host: 'localhost',
-//     dialect: 'mysql'
-//   });
-
-
-// (async function testConnection() {
-//     try {
-//         await sequelize.authenticate();
-//         console.log('Connection has been established successfully.');
-//       } catch (error) {
-//         console.error('Unable to connect to the database:', error);
-//       }
-// })();
+const sequelize = new Sequelize('elena', dbUsername, dbPassword, {
+    host: 'localhost',
+    dialect: 'mysql'
+  });
 
 
-// const User = sequelize.define('User', {
-//     firstName: {
-//         type: DataTypes.STRING,
-//         allowNull: false
-//     },
-//     lastName: {
-//         type: DataTypes.STRING,
-//     },
-//     email: {
-//         type: DataTypes.STRING,
-//         allowNull: false
-//     },
-//     password: {
-//         type: DataTypes.STRING(1234),
-//         allowNull: false
-//     },
-//     isAdmin: {
-//         type: DataTypes.BOOLEAN
-//     },
-//     resetPasswordToken: {
-//         type: DataTypes.STRING(1234)
-//     },
-//     resetPasswordExpires: {
-//         type: DataTypes.DATE
-//     }
-// });
-
-// (async function syncTable() {
-//     await User.sync();
-// })();
+(async function testConnection() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+})();
 
 
-// // const User = mongoose.model('User', new mongoose.Schema({
-// //     email: { 
-// //         type: String, 
-// //         unique: true,
-// //         required: true,
-// //         maxlength: 75
-// //     },
-// //     username: { 
-// //         type: String, 
-// //         unique: true,
-// //         required: true,
-// //         maxlength: 25
-// //      },
-// //     password: { 
-// //         type: String, 
-// //         required: true,
-// //         minlength: 8,
-// //         maxlength: 1024
-// //      },
-// //     isAdmin: { type: Boolean, default: false },
-// //     firstName: { 
-// //         type: String, 
-// //         required: true,
-// //         maxlength: 50
-// //      },
-// //     lastName: { type: String, maxlength: 50 }
-// // }, {timestamp: true}));
+const User = sequelize.define('User', {
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: DataTypes.STRING,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING(1234),
+        allowNull: false
+    },
+    isAdmin: {
+        type: DataTypes.BOOLEAN
+    },
+    resetPasswordToken: {
+        type: DataTypes.STRING(1234)
+    },
+    resetPasswordExpires: {
+        type: DataTypes.DATE
+    }
+});
+
+(async function syncTable() {
+    await User.sync();
+})();
 
 
-// function validateData(user) {
-//     const userSchema = joi.object({
-//         email: joi.string().max(75).required(),
-//         password: joi.string().min(8).max(50).required(),
-//         confirm_password: joi.string().min(8).max(50).required(),
-//         first_name: joi.string().max(50).required(),
-//         last_name: joi.string().max(50)
-//     });
-//     return userSchema.validate(user);
-// }
+function validateData(user) {
+    const userSchema = joi.object({
+        email: joi.string().max(75).required(),
+        password: joi.string().min(8).max(50).required(),
+        confirm_password: joi.string().min(8).max(50).required(),
+        first_name: joi.string().max(50).required(),
+        last_name: joi.string().max(50)
+    });
+    return userSchema.validate(user);
+}
 
-// exports.User = User;
-// exports.validateData = validateData;
+exports.User = User;
+exports.validateData = validateData;
